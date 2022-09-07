2ee2af403e41ed8791070435fab2066f21116755
ElieltonCarvalho <elieltoncarvalho2@gmail.com>
2022-09-07 17:31:26 -0300

    commit do projeto c#

--- 01conceitos/Program.cs
+++ 01conceitos/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace _01conceitos
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar objeto
+
+            Pessoa obj = new Pessoa();
+
+            obj.nome ="Elielton";
+            obj.idade = 24;
+
+            obj.mensagem();
+
+        }
+    }
+}
